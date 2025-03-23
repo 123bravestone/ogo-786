68e36d9dcb6007184d3a0785d4f986980d0ac339
123bravestone <nazamalam15082000@gmail.com>
2025-03-23 15:31:05 +0530

    3:31

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    port: 5173,
-  },
-  plugins: [
-    tailwindcss(), react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      tailwindcss(), react()],
+    define: {
+      'process.env': env, // Load environment variables
+    },
+  };
+
 })
 
 // proxy: {
