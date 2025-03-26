aa620ba4fc7f306baa88c136d41637f3a020a15d
123bravestone <nazamalam15082000@gmail.com>
2025-03-26 09:06:43 +0530

    9:06

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app.use('/api/main', mainRouter)
 app.get('/', (req, res) => {
     res.json({ message: "API is running..." });
 });
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_DB_DATABASE)
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
