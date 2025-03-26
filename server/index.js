da164261bf67eefd89ac46504d7eff956ab647dc
123bravestone <nazamalam15082000@gmail.com>
2025-03-26 06:19:14 +0530

    6;19

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 
 const app = express()
 
-// app.use(cors())
+app.use(cors())
 
 
 app.use(express.json())
@@ -25,19 +25,19 @@ app.use('/api/listing', listingRouter)
 app.use('/api/allshop', allshopRouter)
 app.use('/api/main', mainRouter)
 
-const corsOptions = {
-    origin: "https://offlinego.in", // Allow only your frontend domain
-    methods: "GET, POST, PUT, DELETE, OPTIONS",
-    allowedHeaders: "Content-Type, Authorization"
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: "https://offlinego.in", // Allow only your frontend domain
+//     methods: "GET, POST, PUT, DELETE, OPTIONS",
+//     allowedHeaders: "Content-Type, Authorization"
+// };
+// app.use(cors(corsOptions));
 
 // If using Cloudinary, ensure the API call has CORS headers
 
 app.get('/', (req, res) => {
     res.json({ message: "API is running..." });
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 mongoose.connect(process.env.MONGO_DB_DATABASE)
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
